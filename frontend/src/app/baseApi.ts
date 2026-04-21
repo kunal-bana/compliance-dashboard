b7d183d8f6d9a95596dc88fb75cce957912f9da0
Kunal Bana <kunalbana1007@gmail.com>
2026-04-21 15:42:45 +0530

    removed deployment errors

--- frontend/src/app/baseApi.ts
+++ frontend/src/app/baseApi.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: import.meta.env.VITE_API_URL + "/api",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
     if (token) {
